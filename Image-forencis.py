c3b14b8911704740a6fa3c966563d54b70b5b770
kumarayana <84946101+KuMarayana@users.noreply.github.com>
2021-06-06 05:34:26 -0400

    Add files via upload

--- Image-forencis.py
+++ Image-forencis.py
@@ -8,37 +8,37 @@ from PIL.ExifTags import TAGS
 
 
 def findImages(url):
-      print '[+] finding imagesnya sekarang' + url
-      urlContent = urllib2.urlopen(url).read()
-      soup = BeautifulSoup(urlContent)
-      imgTags = soup.findAll('img')
-      return imgTags
+   print '[+] finding imagesnya sekarang' + url
+   urlContent = urllib2.urlopen(url).read()
+   soup = BeautifulSoup(urlContent)
+   imgTags = soup.findAll('img')
+   return imgTags
 def downloadImage(imgTag):
    try:
-       print '[+] download image now .......'
-       imgSrc = imgTag['src']
-       imgContent = urllib2.urlopen(imgSrc).read()
-       imgFileName = basename(urlsplit(imgSrc)[2])
-       imgFile - open(imgFileName, 'wb')
-       imgFile.write(imgContent)
-       imgFile.close()
-       return imgFIleName
+      print '[+] download image now .......'
+      imgSrc = imgTag['src']
+      imgContent = urllib2.urlopen(imgSrc).read()
+      imgFileName = basename(urlsplit(imgSrc)[2])
+      imgFile - open(imgFileName, 'wb')
+      imgFile.write(imgContent)
+      imgFile.close()
+      return imgFIleName
    except:
-       return ''
+      return ''
 def testForExif(imgFileName):
    try:
        exifData = []
        imgFile = image.open(imgFileName)
        info = imgFile._getexif()
        if info:
-          for (tag, value) in info.items():
+         for (tag, value) in info.items():
             decode = TAGS.get(tag, tag)
             exifData[decoded] = value
-          exifGPS = exifData['GPSinfo']
-          if exifGPS:
-             print '[*] ' + imgFileName + ' gps for info guys'
-       except:
-          pass
+            exifGPS = exifData['GPSinfo']
+            if exifGPS:
+               print '[*] ' + imgFileName + ' gps for info guys'
+   except:
+      pass
 def main():
    parser = optparse.OptionsParser('usage%prog "+\ "-u <tagret url>')
    parser.add_option('-u', dest='url', type='string', help='specify url addres')
